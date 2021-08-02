81f3de8de93c7e8171fb83cf8029e43980d16ab9
miyushan <smiushan2015@gmail.com>
2021-08-02 20:46:16 +0530

    login form coverted to a function component

--- src/Parts/LoginForm.js
+++ src/Parts/LoginForm.js
@@ -1,4 +1,4 @@
-import React, { Component} from 'react';
+import React, { useState, useEffect} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/LoginForm.css';
 import axios from 'axios';
@@ -6,113 +6,69 @@ import { Route, Redirect } from "react-router-dom";
 
 import {Form, Button} from "react-bootstrap";
 
-export default class LoginForm extends Component {
+export default function LoginForm (){
 
-    constructor(props){
-        super(props);
+    const [customers, setCustomers] = useState([]);
+    const [contactNumber, setContactNumber] = useState('');
+    const [password, setPassword] = useState('');
+    const [isValidUser, setIsValidUser] = useState(false);
+    
 
-        this.onChangeContactNumber = this.onChangeContactNumber.bind(this);
-        this.onChangePassword = this.onChangePassword.bind(this);
-        this.onSubmit = this.onSubmit.bind(this);
-
-        this.state = {
-            firstName: '',
-            lastName: '',
-            gender: '',
-            contactNumber: '',
-            branchName: '',
-            password: '',
-            address: '',
-            isValidUser: false
-        }
+    const onChangeContactNumber = (e) => {
+        setContactNumber(e.target.value);
     }
 
-    onChangeContactNumber(e){
-        this.setState({
-            contactNumber: e.target.value
+    const onChangePassword = (e) => {
+        setPassword(e.target.value);
+    }
+
+    useEffect(() => {
+        axios.get('http://localhost/database_project/get_Customer_details.php')
+        .then (res =>{
+            // console.log(res.data);
+            setCustomers(res.data)
         })
-    }
+    }, []);
 
-    onChangePassword(e){
-        this.setState({
-            password: e.target.value
-        })
-    }
-
-    onSubmit(e){
+    const onSubmit =(e)=>{
         e.preventDefault();
 
-        axios.get('http://localhost/database_project/get_Customer_Details.php')
-        .then(res => res.data)
-        .then((res) => {
+        customers.forEach(customer => {
 
-            res.forEach(elements => {
-                console.log(elements.Contact_Number + "\t" + this.state.contactNumber);
-                console.log(elements.Password + "\t" + this.state.password);
-                console.log();
+            if(contactNumber === customer.Contact_Number && password === customer.Password){
+                setIsValidUser(true);
+                console.log("Login success!");
+            }
 
-                if((elements.Contact_Number === this.state.contactNumber) && (elements.Password===this.state.password)){
-                    
-                    this.setState({
-                        firstName: elements.First_Name,
-                        lastName: elements.Last_Name,
-                        gender: elements.Gender,
-                        contactNumber: elements.Contact_Number,
-                        branchName: elements.Branch_Name,
-                        password: elements.Password,
-                        address: elements.Address,
-                        isValidUser: true
-                    })
-                    console.log("found");
-
-                    const obj = {
-                        firstName: elements.Last_Name,
-                        lastName: elements.First_Name,
-                        gender: elements.Gender,
-                        contactNumber: elements.Contact_Number,
-                        branchName: elements.Branch_Name,
-                        password: elements.Password,
-                        address: elements.Address,
-                    }
-                    const obj_json = JSON.stringify(obj);
-                    console.log(obj);
-                    
-                    axios.post('http://localhost/database_project/user_details.php', obj_json)
-                    .then(res => {
-                        console.log(res.data);
-                    });
-                }
-            });
-        });
-
+        })
     }
 
 
-    render() {
-        return(
-            <>
-                <Form className="login-form" onSubmit={this.onSubmit} method="post">
     
-                    <Form.Group className="mb-3" controlId="formBasicPhoneNumber">
-                        <Form.Control className="login-input" type="text" placeholder="Phone Number"  variant="success" name="contactNumber" value={this.state.contactNumber} onChange={this.onChangeContactNumber}/>
-                        <Form.Text className="text-muted">
-                        </Form.Text>
-                    </Form.Group>
-    
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Control className="login-input" type="password" placeholder="Password"  variant="success" name="password" value={this.state.password}  onChange={this.onChangePassword}/>
-                    </Form.Group>
-    
-                    <Button href="" className="login-submit-btn login-input" type="submit"  variant="success">LOG IN</Button>
+    return(
+        <>
+            <Form className="login-form" onSubmit={onSubmit} method="post">
 
-                    <Route>
-                        {this.state.isValidUser ? <Redirect to="/home" /> : null} 
-                    </Route>
+                <Form.Group className="mb-3" controlId="formBasicPhoneNumber">
+                    <Form.Control className="login-input" type="text" placeholder="Phone Number"  variant="success" name="contactNumber" value={contactNumber} onChange={onChangeContactNumber}/>
+                    <Form.Text className="text-muted">
+                    </Form.Text>
+                </Form.Group>
 
-                </Form>
-            </>
-        );
-    }
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Control className="login-input" type="password" placeholder="Password"  variant="success" name="password" value={password}  onChange={onChangePassword}/>
+                </Form.Group>
+
+                <Button href="" className="login-submit-btn login-input" type="submit"  variant="success">LOG IN</Button>
+
+                <Route>
+                    {isValidUser ? <Redirect to="/home" /> : null} 
+                </Route>
+
+            </Form>
+        </>
+    );
+    
     
 }
 
