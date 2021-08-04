f5cdf312b19f95e829b49858fb7f25c1778f5a3a
miyushan <smiushan2015@gmail.com>
2021-08-05 00:15:54 +0530

    tries to include clicked products to an array

--- src/Parts/CardItem.js
+++ src/Parts/CardItem.js
@@ -5,10 +5,43 @@ import {Card, Container, Row, Col, Button} from "react-bootstrap";
 
 export default class CardItem extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            clickedItems: [],
+        }
+
+        this.onClick = this.onClick.bind(this);
+        // this.componentDidMount = this.componentDidMount.bind(this);
+    }
+
+    // componentDidMount(){
+    //     this.setState({
+    //         isClicked: false,
+    //     })
+    // }
+
+    onClick(key){
+        console.log(key);
+        
+        
+        // this.clickedItems.push(key)
+        // console.log(this.clickedItems[0]);
+        // clickedProducts.push(key);
+
+        // if(this.isClicked){
+        //     console.log({key} + 'already clicked');
+        // }else{
+        //     console.log('new product selected')
+        // }
+
+    }
+
 
     render(){
 
-        console.log(this.props.weight);
+        // console.log(this.props.weight);
 
         return(
             <>
@@ -22,7 +55,7 @@ export default class CardItem extends Component {
                                 <Col className="price">Rs. {this.props.price}</Col>
                             </Row>
                         </Container>
-                        <Button className="btn-cart">Add To Cart</Button>
+                        <Button onClick={()=>{this.onClick(this.props.name)}} className="btn-cart">Add To Cart</Button>
                     </Card.Body>
                 </Card>
                 
