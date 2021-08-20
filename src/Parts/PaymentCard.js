503fab63975d6d7046650fb959725e3c4b32870f
SMiyushan <smiyushan2020@gmail.com>
2021-08-21 01:17:09 +0530

    payment card works

--- src/Parts/PaymentCard.js
+++ src/Parts/PaymentCard.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useState, useContext } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/PaymentCard.css';
 import {Form, Container, Button, Row, Col} from "react-bootstrap";
@@ -6,15 +6,15 @@ import { LoginDetailsContext } from '../Context/LoginDetailsContext';
 
 function PaymentCard(){
 
-    const { isLogedIn, setIsLogedIn } = useContext(LoginDetailsContext);
+    const { setIsLogedIn } = useContext(LoginDetailsContext);
 
   
     const [name, setName] = useState('');
     const [address, setAddress] = useState('');
     const [cardNumber, setCardNumber] = useState('');
-    const [month, setMonth] = useState('');
+    // const [month, setMonth] = useState('');
     const [date, setDate] = useState('');
-    const [cvc, setcvc] = useState('');
+    const [cvv, setcvc] = useState('');
 
     const onChangeName = (e) => {
         setName(e.target.value);
@@ -28,36 +28,37 @@ function PaymentCard(){
         setCardNumber(e.target.value);
     }
 
-    const onChangeMonth = (e) => {
-        setMonth(e.target.value);
-    }
+    // const onChangeMonth = (e) => {
+    //     setMonth(e.target.value);
+    // }
 
     const onChangeDate = (e) => {
         setDate(e.target.value);
     }
 
-    const onChangecvc = (e) => {
+    const onChangecvv = (e) => {
         setcvc(e.target.value);
     }
 
     const onPayNow =(e)=>{
-        localStorage.removeItem('cartDetails');
-        localStorage.removeItem('priceDetails'); 
-        console.log('hello')
-        e.preventDefault(); 
-        setIsLogedIn(false); 
-        setName('');
-        setAddress('');
-        setCardNumber('');
-        setDate('');
-        setMonth('');
-        setcvc('');
+        if(name && address && cardNumber && date && cvv){
+            localStorage.removeItem('cartDetails');
+            localStorage.removeItem('priceDetails'); 
+            console.log('hello')
+            // e.preventDefault(); 
+            setIsLogedIn(false); 
+            setName('');
+            setAddress('');
+            setCardNumber('');
+            setDate('');
+            setcvc('');
+        }
     }
 
     return(
         <>
             <Container className="form-container">
-                <Form className="payment-form">
+                <Form className="payment-form" onSubmit={onPayNow}>
                     <Row className="mt-1 mb-4">
                         <Form.Group as={Col} controlId="formGridName">
                         <Form.Label className="payment-field-title">Name On The Card</Form.Label>
@@ -79,20 +80,20 @@ function PaymentCard(){
                         <Col xs={5}>
                             <Form.Group  controlId="formGridExpireDate">
                             <Form.Label className="payment-field-title">Expiration Date</Form.Label>
-                            <Form.Control className="login-input btn-square" placeholder="00/00" value={month} onChange={onChangeMonth}/>
+                            <Form.Control className="login-input btn-square" placeholder="00/00" value={date} onChange={onChangeDate}/>
                             </Form.Group>
                         </Col>
                         <Col xs={{ span: 4, offset: 3 }}>
                             <Form.Group controlId="formGridCVV">
                             <Form.Label className="payment-field-title">CVV</Form.Label>
-                            <Form.Control className="login-input btn-square" placeholder="123" value={cvc} onChange={onChangecvc}/>
+                            <Form.Control className="login-input btn-square" placeholder="123" value={cvv} onChange={onChangecvv}/>
                             </Form.Group>
                         </Col>
                         
                     </Row>
 
                     <div className=" mb-1 ">
-                        <Button onSubmit={onPayNow} className="login-submit-btn login-input pay-now-btn" type="submit">PAY NOW</Button>
+                        <Button href="" className="login-submit-btn login-input pay-now-btn" type="submit">PAY NOW</Button>
                     </div>
 
                 </Form>
