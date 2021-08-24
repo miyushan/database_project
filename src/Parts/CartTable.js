b110db804aa2b2a87d095a22790696f05d459a33
SMiyushan <smiyushan2020@gmail.com>
2021-08-24 19:16:37 +0530

    okay for now

--- src/Parts/CartTable.js
+++ src/Parts/CartTable.js
@@ -1,14 +1,19 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/CartTable.css';
-import {Table, Container} from "react-bootstrap";
+import {Table, Container, Col, Row} from "react-bootstrap";
 import item1 from '../files/product-images/1_Beet.png';
 import { CartContext } from '../Context/CartContext';
 import { ReactComponent as Remove } from '../files/icons/remove-from-cart.svg';
+import { ReactComponent as Reduce } from '../files/icons/reduce.svg';
+import { ReactComponent as Increase } from '../files/icons/increase.svg';
 
 function CartTable () {
 
     const { cartProducts, removeFromCart } = useContext(CartContext);
+
+    const [quantity, setQuantity] = useState(0);
+    const [price, setPrice] = useState(0);
 
     // const [id, setCustomers] = useState([]);
     // const [name, setContactNumber] = useState('');
@@ -24,9 +29,15 @@ function CartTable () {
     //     setPassword(e.target.value);
     // }
 
-    useEffect(() => {
-
-    },[])
+    const increase = (weight, price) => {
+        let tempQ = quantity;
+        tempQ = tempQ + 10;
+        setQuantity(tempQ);
+    }
+    
+    const reduce = (weight, price) => {
+        console.log(weight)
+    }
 
     return(
         <>
@@ -40,6 +51,7 @@ function CartTable () {
                             <tr className="table-header">
                                 <th className="table-column table-column-1">Product ID</th>
                                 <th className="table-column table-column-2">Product</th>
+                                <th className="table-column table-column-3">Set Quantity</th>
                                 <th className="table-column table-column-3">Quantity</th>
                                 <th className="table-column table-column-4">Remove</th>
                                 <th className="table-column table-column-4">Price</th>
@@ -53,9 +65,15 @@ function CartTable () {
                                     <tr key={product.id}>
                                         <td>{product.id}</td>
                                         <td className="left-image"><img src={item1} alt="product" className="img-product"/>{product.Name}</td>
-                                        <td>{product.Weight} Kg</td>
+                                        <td className="add-reduce">
+                                            <Row>
+                                                <Col><Increase onClick={()=>{increase(product.Weight, product.Price)}} className="success change-weight" height="22px"/></Col>
+                                                <Col><Reduce onClick={()=>{reduce(product.Weight, product.Price)}} className="success change-weight" height="22px"/></Col>
+                                            </Row>
+                                        </td>
+                                        <td>{quantity} Kg</td>
                                         <td className="">
-                                            <a className="remove-from-cart-btn" onClick={(e)=>{ removeFromCart(product.id, product.Price); e.preventDefault();}}><Remove className="success remove-from-cart" height="22px"/></a>
+                                            <Remove className="success remove-from-cart" onClick={(e)=>{ removeFromCart(product.id, product.Price); e.preventDefault();}} height="22px"/>
                                         </td>
                                         <td className="price-col">Rs {product.Price}</td>
                                     </tr>
