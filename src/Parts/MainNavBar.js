068a08decc048e8f255c52b161bc3d138808770f
SMiyushan <smiyushan2020@gmail.com>
2021-09-01 12:19:25 +0530

    cart items are shown above cart icon

--- src/Parts/MainNavBar.js
+++ src/Parts/MainNavBar.js
@@ -1,13 +1,17 @@
-import React, { useState} from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/MainNavBar.css';
-import {Navbar, Container, Button} from "react-bootstrap";
+import {Navbar, Container, Button, Col} from "react-bootstrap";
 
+import { CartContext } from '../Context/CartContext';
 import { ReactComponent as Cart } from '../files/shopping-cart-solid.svg';
 
 function MainNavBar(){
-
+    const { cartProducts } = useContext(CartContext);
     const[scroll, setScroll] = useState(true);
+    useEffect (() => {
+
+    },[cartProducts])
 
     const style = {
         backgroundColor : "#3b3737f8",
@@ -28,15 +32,24 @@ function MainNavBar(){
 
     return(    
         <Navbar style={scroll ? style : null} className="outer-nav">
-            <Container className="main-nav">
-                <Navbar.Brand className="nav-title-2" href="home">HOME</Navbar.Brand>
-                <Navbar.Brand className="nav-title-2" href="products">PRODUCTS</Navbar.Brand>
-                <Navbar.Brand className="nav-title-2" href="about-us">ABOUT US</Navbar.Brand>
-                <Navbar.Brand className="nav-title-2" href="contact-us">CONTACT US</Navbar.Brand>
-                <Navbar.Brand className="nav-title-2" href="branches">BRANCHES</Navbar.Brand>
-                <div className="cart to-cart-btn">
-                    <Button href="cart" className="ignore-btn"  variant="success"><Cart className="cart-icon" height="25px"/></Button>
-                </div>
+            <Container className="main-nav d-flex justify-content-center align-items-center">
+                    <Col></Col>
+                    <Col className="main-nav-items d-flex justify-content-center align-items-center m-0 p-0">
+                        <Navbar.Brand className="nav-title-2" href="about-us">ABOUT US</Navbar.Brand>
+                        <Navbar.Brand className="nav-title-2" href="products">PRODUCTS</Navbar.Brand>
+                        <Navbar.Brand className="nav-title-2" href="home">HOME</Navbar.Brand>
+                        <Navbar.Brand className="nav-title-2" href="branches">BRANCHES</Navbar.Brand>
+                        <Navbar.Brand className="nav-title-2" href="contact-us">CONTACT US</Navbar.Brand>
+                    </Col>
+                    <Col className="d-flex justify-content-end align-items-center">
+                        <div className="cart to-cart-btn">
+                            <Button href="cart" className="ignore-btn"  variant="success"><Cart className="cart-icon" height="25px"/></Button>
+                            {cartProducts.length !== 0 ? <div>
+                                <div className="cartProductExist"><div className="cartProductCountRel"></div></div>
+                                <div className="cartProductCount">{cartProducts.length}</div>
+                            </div> : null }
+                        </div>
+                    </Col>
             </Container>    
         </Navbar>
 
