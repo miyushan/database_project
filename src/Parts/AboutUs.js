3b241df520a7402c3b2f70dba3aba354453960fb
Miyushan <smiushan2015@gmail.com>
2022-01-28 10:48:30 +0530

    changed about us

--- src/Parts/AboutUs.js
+++ src/Parts/AboutUs.js
@@ -18,7 +18,7 @@ function AboutUs(){
                         </Col>
                         <Col className="aboutUs-col-3 d-flex">
                             <div className="aboutUs-description">
-                                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim
+                                We are VegiDeals and here to fulfill your needs of Vegetables virtually. You can visit our site, choose whatever you need, and get them to your doorstep with our efficient service. We are a team that considers client satisfaction. So you can feel the best food ordering experience.
                             </div>
                         </Col>
                     </Row>
