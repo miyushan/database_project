c988ca2ddaac2367131ae56cbefc228fa1bda42a
Miyushan <smiushan2015@gmail.com>
2022-10-30 03:12:11 +0530

    login page code improved

--- Client/src/Components/Login.js
+++ Client/src/Components/Login.js
@@ -1,44 +1,37 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/Login.css';
 import { Col, Container, Row, Button } from "react-bootstrap";
-import cover from '../Media/marisol-benitez-QvkAQTNj4zk-unsplash.jpg';
 import LoginForm from '../Components/LoginForm';
 import { ReactComponent as Admin } from '../Media/icons/users-cog-solid.svg';
 
 function Login() {
     return (
         <>
-            <div id="login">
-                <Container className="outer-login">
-                    <Row>
+            <Container id="login" fluid>
+                <Row>
+                    <Col xs={12} md={6} className="b_login-left d-flex align-items-center justify-content-center">
+                        <h1 className="b_login-header">Online Vegetable<br />Ordering System</h1>
+                        <div className="b_admin_login">
+                            <a href="db/login" className=""><Admin className="b_admin-login-icon" height="20px" /></a>
+                        </div>
+                    </Col>
+                    <Col xs={12} md={6} className="b_login-right d-flex align-items-center justify-content-center">
 
-                        <Col className="login-left">
-                            <img className="login-Image" src={cover} alt="cover" />
-                            <div className="align">
-                                <h1 className="login-header text-left add-curser">Online Vegetable<br />Ordering System</h1>
-                            </div>
-                            <div className="admin-login">
-                                <a href="db/login" className="admin-login-btn"><Admin className="admin-login-icon" height="20px" /></a>
-                            </div>
-                        </Col>
-                        <Col className="login-right d-flex justify-content-center">
-                            <div className="create-account">
-                                <Button href="register" className="create-account-btn" type="submit" variant="success">Create A New Account</Button>
-                            </div>
-                            <Row className="login-row-0 text-left">
-                                <Col className="d-block justify-content-center login-row-4">
-                                    <div className="login-title login-title-1 add-curser">Welcome to VegiDeals</div>
-                                    <div className="login-title login-title-2 add-curser">We make you smart</div>
-                                </Col>
-                            </Row>
-                            <Row className="login-row-0">
-                                <Col className="d-flex justify-content-center login-row-5"><LoginForm /></Col>
-                            </Row>
-                        </Col>
+                        <Row className="b_login-row">
+                            <Col className="d-block justify-content-center">
+                                <div className="b_login-title b_login-title-1 b_add-curser">Welcome to VegiDeals</div>
+                                <div className="b_login-title b_login-title-2 b_add-curser">We make you smart</div>
+                                <LoginForm />
+                            </Col>
+                        </Row>
 
-                    </Row>
-                </Container>
-            </div>
+                        <div className="create-account">
+                            <Button href="register" className="create-account-btn" type="submit" variant="success">Create A New Account</Button>
+                        </div>
+                    </Col>
+
+                </Row>
+            </Container>
         </>
     );
 }
